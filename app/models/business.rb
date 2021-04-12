ace52b2321ea5604c888842d819e7d61d3d25940
Jacob Hampton <jhampton792@gmail.com>
2021-04-12 14:30:28 -0500

    Added association and validation for businesses.

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,2 +1,11 @@
 class Business < ApplicationRecord
+    has_secure_password
+
+    validates :name, :street_address, :city, :state, :phone, presence: true
+    validates :email, uniqueness: { case_sensitive: false }
+    validates :password, length: { in: 10..30, wrong_length: "Your password must be within 10 and 30 characters" }
+
+    has_many :bid_responses
+    has_many :experiences, through: :bid_responses
+    has_many :reviews, through: :experiences
 end
